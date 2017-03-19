eb2751cbd7557c660ee53028502a668b9bf64919
Samuel Turner <samuelturner@Samuels-MacBook-Pro.local>
2017-03-18 17:42:39 -0700

    added code to generate paths file for client

--- lib/lieutenant_governor/generators/js_route_helpers.rb
+++ lib/lieutenant_governor/generators/js_route_helpers.rb
@@ -8,6 +8,14 @@ module LieutenantGovernor
       # assume function exists to translate hash to javascript text
       # using Thor, open up a file, and then write the javascript text to
       # the file
+
+      def self.generate_paths_file
+        routes = Rails.application.routes.routes
+        route_table = LieutenantGovernor::Routing::Extractor.extract(routes)
+        # js_text = call some method
+        File.open('/client/paths.js', 'w') {|file| file.truncate(0) }
+        append_to_file '/client/paths.js', js_text
+      end
     end
   end
 end
