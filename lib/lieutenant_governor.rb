45e2cbe7a7db9124097fdabe2715264e7056ad9f
jordanlittell <jordanlittell@yahoo.com>
2017-03-18 18:39:49 -0700

    Simplify interface

--- lib/lieutenant_governor.rb
+++ lib/lieutenant_governor.rb
@@ -1,6 +1,12 @@
 require "lieutenant_governor/version"
-require "lieutenant_governor/route_extractor"
+require 'lieutenant_governor/generators/js_route_helpers'
 
 module LieutenantGovernor
-  
+
+  class Generator
+    def self.generate
+      Generators::JsRouteHelpers.generate_paths_file
+    end
+  end
+
 end
